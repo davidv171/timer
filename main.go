ea407e4a4992e1629a82d1815d8d548e566e6762
davidv171 <david.vuckovic7@gmail.com>
2020-02-12 20:12:57 +0100

    Change package name

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"flag"
-	"github.com/davidv171/timer/custom"
+
 	"github.com/davidv171/timer/countdown"
+	"github.com/davidv171/timer/custom"
 )
 
 func main() {
@@ -11,14 +12,14 @@ func main() {
 	countdownptr := flag.Int("timer", -1, "Set timer value")
 	customf := flag.String("custom", "", "Run custom command after it's over")
 	lolcat := flag.Bool("lolcat", false, "Pipe to lolcat?")
-	timerptr := flag.Bool("timer",false,"Perform a timer instead of a countdown")
+	timerptr := flag.Bool("timer", false, "Perform a timer instead of a countdown")
 
 	flag.Parse()
 
-	if(*countdownptr == -1) {
+	if *countdownptr == -1 {
 
 	}
-	countdown.Startt(countdownptr, lolcat)
+	countdown.Countdown(countdownptr, lolcat)
 	custom.Runc(customf)
 
 }
